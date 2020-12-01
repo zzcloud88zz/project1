9e154e367db521bfb52c45a28e16d78d25b45191
shengrong <shengrong_88@hotmail.com>
2020-12-01 12:02:46 +0800

    styling done pcmob2 slide 91

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, YellowBox } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi!!! Ni Hao Ma!?</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.fillerContainer}></View>
+
+      <View style={styles.titleContainer}>
+        <Text style={[styles.baseStyle, styles.text, {fontStyle: "normal"}]}>
+          HELLO!!!
+        </Text>
+        <Text style={[styles.baseStyle, {color: "red", fontWeight: "bold"}]}>
+          I AM INEVITABLE. *snap
+        </Text>
+      </View>
+
+        <View style={styles.fillerContainer}></View>
+
+        <StatusBar style="auto" />
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'powderblue',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  baseStyle: {
+    fontStyle: "italic",
+    textAlign: "center",
+  },
+  text: {
+    color: "maroon",
+    fontSize: "36",
+  },
+  titleContainer: {
+    backgroundColor: 'orange',
+    width: "60%",
+    flex: 2,
+    padding: 10,
+    margin: 10,
+    justifyContent: "center",
+    borderRadius: 20,
+  },
+  fillerContainer: {
+    flex: 1,
+    width: "50%",
+    backgroundColor: 'skyblue',
   },
 });
